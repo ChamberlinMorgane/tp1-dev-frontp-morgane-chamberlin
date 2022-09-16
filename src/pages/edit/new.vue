8eb51f5b8c27109c749ea78995de83f77b44e830
Morgane <morgane.chamberlin@edu.univ-fcomte.fr>
2022-09-16 11:31:07 +0200

    Création de New.vue

---
+++ src/pages/edit/new.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import FormulaireOffreMaison from "../../components/FormulaireOffreMaison.vue";
+
+</script>
+<template>
+    <div>
+        <FormulaireOffreMaison />
+        <h2>test</h2>
+    </div>
+</template>
