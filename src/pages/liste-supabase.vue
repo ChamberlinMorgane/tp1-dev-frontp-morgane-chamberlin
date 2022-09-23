0aa8860444b4e3ced35aa0b763ba5ef97fc18dd1
Morgane <morgane.chamberlin@edu.univ-fcomte.fr>
2022-09-23 13:13:41 +0200

    mise en place de la vue liste supabase

---
+++ src/pages/liste-supabase.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="p-2">
+        <h1 class="text-2xl">Page Liste</h1>
+        <div v-for="maisons in maisons" :key="maisons.nom">
+            <card class="w-1/2" v-bind="maisons" />
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import card from "../components/card.vue";
+
+import { supabase } from "../supabase";
+console.log("supabase :", supabase); // pour vérifier et "garder" supabase dans le code
+
+const maison = [supabase]
+const { data: maisons, error } = await supabase
+    .from('Maison')
+    .select('*')
+
+
+console.log("Maison", maison)
+
+
+//globalThis.supabase = supabase//
+</script>
