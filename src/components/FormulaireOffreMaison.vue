2790b6e2652f2d0d6f42006560776796ecb3113e
Morgane <morgane.chamberlin@edu.univ-fcomte.fr>
2022-09-26 10:39:34 +0200

    nouvelle fonction dans offre

--- src/components/FormulaireOffreMaison.vue
+++ src/components/FormulaireOffreMaison.vue
@@ -1,7 +1,17 @@
 <script setup lang="ts">
+import { supabase } from '@/supabase';
+import { useRouter } from "vue-router";
 import { ref } from "@vue/reactivity";
 import Card from "./card.vue"
-const maison = ref({ nom: "Paris", price: 600, favoris: false, txt: "Maison haut de gamme", nbbath: 4, nbsize: "282 m²", img: "/house.jpg" });
+const maison = ref({
+    nom: "Paris", price: 600, //favoris: false
+    txt: "Maison haut de gamme", nbbath: 4, nbsize: "282 m²", img: "/house.jpg"
+});
+
+async function upsertMaison(dataForm, node) {
+    const { data, error } = await supabase.from("Maison").upsert(dataForm);
+    if (error) node.setErrors([error.message])
+}
 </script>
 <template>
     <div>
@@ -10,7 +20,7 @@ const maison = ref({ nom: "Paris", price: 600, favoris: false, txt: "Maison haut
             <Card class="w-1/2 m-auto mt-10" v-bind="maison" />
         </div>
         <div class="p-10 ">
-            <FormKit type="form" v-model="maison" :config="{
+            <FormKit @submit="upsertMaison" type="form" v-model="maison" :config="{
             classes: {
             input: 'p-1 rounded border-gray-500 shadow-sm border',
             label: 'text-gray-600',
@@ -19,7 +29,8 @@ const maison = ref({ nom: "Paris", price: 600, favoris: false, txt: "Maison haut
 
                 <FormKit name="nom" label="Lieu de la maison" />
                 <FormKit name="price" label="Le prix de la maison" type="number" />
-                <FormKit name="favoris" label="mettre en favoris" type="checkbox" wrapper-class="flex" />
+                <!--
+                <FormKit name="favoris" label="mettre en favoris" type="checkbox" wrapper-class="flex" />-->
             </FormKit>
         </div>
     </div>
